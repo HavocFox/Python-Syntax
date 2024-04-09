ed6b8baeee2e0fb0a8c893a0abd054dcbcea7e3b
HavocFox <duskydraws@gmail.com>
2024-04-09 14:58:33 -0400

    ooo testing ooo

---
+++ danceMoves.py
@@ -0,0 +1,13 @@
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+mood = input("How do you feel today? ")
+
+if mood == "happy":
+    print("That's great to hear!")
+if mood == "sad":
+    print("I hope your day gets better!")
