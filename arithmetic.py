e28864c8e2e94e28abf61f59209a3cf0faf1ac9a
HavocFox <duskydraws@gmail.com>
2024-04-09 15:31:23 -0400

    full assignment hopefully

---
+++ arithmetic.py
@@ -0,0 +1,18 @@
+#grocery store math
+
+bread = 3.00
+peanut_butter = 2.50
+jelly = 1.30
+
+total_cost = bread + jelly + peanut_butter
+print("Total cost is:")
+print(total_cost)
+
+#bank interest
+
+bank = 10000
+interest = 0.07
+end_total = (10000*0.07) + 10000
+
+print("Your bank account has this much at the end of the year:")
+print(end_total)
---
+++ dataTypes.py
@@ -0,0 +1,14 @@
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+#variable_a is a string.
+#variable_b is an integer.
+#variable_c is a float.
+#variable_d is a boolean.
+
+print (type(variable_a))
+print (type(variable_b))
+print (type(variable_c))
+print (type(variable_d))
